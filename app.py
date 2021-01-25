6f48181a7afbef93a6109999d1ba497afc6b213a
Art Tucker <art.w.tucker@gmail.com>
2021-01-25 00:33:15 -0800

    App setup
    Dependencies
    SQLAlchemy setup
    initiaate classes
    start session

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+# Standard Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-def hello_world():
-    name = input("What's your name?")
-    return 'Hello, '+name+'!'
+# SQLAlchemy dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify()
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
