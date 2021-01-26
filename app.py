fd7601db2f35dc4b6caee2481276ffa3d6485ab9
Art Tucker <art.w.tucker@gmail.com>
2021-01-25 21:40:47 -0800

    write welcome route

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# Flask dependency
 from flask import Flask, jsonify()
 
+# SQLite setup
 engine = create_engine("sqlite:///hawaii.sqlite")
-
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -21,4 +22,18 @@ Station = Base.classes.station
 
 session = Session(engine)
 
+# Flask setup
 app = Flask(__name__)
+
+# Welcome route
+@app.route("/")
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
