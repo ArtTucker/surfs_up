ddae90e9674fac073f20987e51740be9ea97a0ba
Art Tucker <art.w.tucker@gmail.com>
2021-01-25 23:46:48 -0800

    stations route

--- app.py
+++ app.py
@@ -46,3 +46,10 @@ def precipitation():
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
+
+# Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
